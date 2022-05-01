6440051765e918803ce4b19c905ecbe0be4df9f6
sjvijay95 <sjvijay95@gmail.com>
2022-05-02 00:31:16 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumMavenTest.java
@@ -0,0 +1,13 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumMavenTest {
+	
+	@Test
+	public void appiumMaven()
+	{
+		System.out.println("AppiumMaven");
+	}
+
+}
